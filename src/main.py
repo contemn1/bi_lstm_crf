9394f867a273c8201918f2dc3f2fb41ee7ce5829
contemn1 <xunjiez614218@gmail.com>
2017-04-26 04:47:58 -0400

    fix bugs in main

--- src/main.py
+++ src/main.py
@@ -210,6 +210,14 @@ def main():
                 tensorboard_character_embeddings.tensor_name = model.character_embedding_weights.name
                 character_list_file_path = os.path.join(model_folder, 'tensorboard_metadata_characters.tsv')
                 tensorboard_character_embeddings.metadata_path = os.path.relpath(character_list_file_path, '..')
+                character_list_file = codecs.open(character_list_file_path, 'w', 'UTF-8')
+                for character_index in range(dataset.alphabet_size):
+                    if character_index == dataset.PADDING_CHARACTER_INDEX:
+                        character_list_file.write('PADDING\n')
+                    else:
+                        character_list_file.write(
+                            '{0}\n'.format(dataset.index_to_character[character_index]))
+                character_list_file.close()
 
             projector.visualize_embeddings(embedding_writer, embeddings_projector_config)
 
@@ -218,15 +226,6 @@ def main():
             for token_index in range(dataset.vocabulary_size):
                 token_list_file.write('{0}\n'.format(dataset.index_to_token[token_index]))
             token_list_file.close()
-
-            character_list_file = codecs.open(character_list_file_path, 'w', 'UTF-8')
-            for character_index in range(dataset.alphabet_size):
-                if character_index == dataset.PADDING_CHARACTER_INDEX:
-                    character_list_file.write('PADDING\n')
-                else:
-                    character_list_file.write('{0}\n'.format(dataset.index_to_character[character_index]))
-            character_list_file.close()
-
 
             # Initialize the model
             sess.run(tf.global_variables_initializer())
